7adb2de0a4c00908e0035c267f81c3e1cc5f2e7a
samu63 <s.corrado@crif.com>
2014-08-24 22:38:50 +0200

    final version

---
+++ server.R
@@ -0,0 +1,163 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(psych)
+
+shinyServer(function(input, output) {
+  
+  options(warn=-1)
+  
+  bs <- reactive({
+    x <- input$textarea.in
+    x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
+    result <- describe(x)[2:13]
+    row.names(result) <- ""
+    return(result)
+  })
+  
+  
+  
+  makedistPlot <- function(){
+    x <- input$textarea.in
+    x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
+    
+    x <- x[!is.na(x)]
+    
+    simple.bincount <- function(x, breaks) {
+      nx <- length(x)
+      nbreaks <- length(breaks)
+      counts <- integer(nbreaks - 1)
+      for (i in 1:nx) {
+        lo <- 1
+        hi <- nbreaks
+        if (breaks[lo] <= x[i] && x[i] <= breaks[hi]) {
+          while (hi - lo >= 2) {
+            new <- (hi + lo) %/% 2
+            if(x[i] > breaks[new])
+              lo <- new
+            else
+              hi <- new
+          }
+          counts[lo] <- counts[lo] + 1
+        }
+      }
+      return(counts)
+    }
+    
+    nclass <- nclass.FD(x)
+    breaks <- pretty(x, nclass)
+    counts <- simple.bincount(x, breaks)
+    counts.max <- max(counts)
+    
+    h <- hist(x, las=1, breaks="FD", xlab= "Red vertical line shows the mean.",
+              ylim=c(0, counts.max*1.2), main="", col = "cyan")
+    rug(x)
+    abline(v = mean(x, na.rm=T), col = "red", lwd = 2)
+    xfit <- seq(min(x, na.rm=T), max(x, na.rm=T))
+    yfit <- dnorm(xfit, mean = mean(x, na.rm=T), sd = sd(x, na.rm=T))
+    yfit <- yfit * diff(h$mids[1:2]) * length(x)
+    lines(xfit, yfit, col = "blue", lwd = 2)
+  }
+  
+  
+  output$distPlot <- renderPlot({
+    print(makedistPlot()) 
+  })
+  
+  
+  
+  makeboxPlot <- function(){
+    x <- input$textarea.in
+    x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
+    boxplot(x, horizontal=TRUE, xlab= "Mean and +/-1 SD are displayed in red.")
+    stripchart(x, pch = 16, add = TRUE)
+    points(mean(x, na.rm=T), 0.9, pch = 18, col = "red", cex = 2)
+    arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) + sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
+    arrows(mean(x, na.rm=T), 0.9, mean(x, na.rm=T) - sd(x, na.rm=T), length = 0.1, angle = 45, col = "red")
+  }
+  
+  
+  output$boxPlot <- renderPlot({
+    print(makeboxPlot()) 
+  })
+  
+  
+  
+  testnorm <- reactive({
+    x <- input$textarea.in
+    x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
+    list(ks.test(scale(x), "pnorm"), shapiro.test(x))
+  })
+  
+  
+  
+  makeqqPlot <- function(){
+    x <- input$textarea.in
+    x <- as.numeric(unlist(strsplit(x, "[\n, \t]")))
+    qqnorm(x, las=1)
+    qqline(x, col=2)
+  }
+  
+  output$qqPlot <- renderPlot({
+    print(makeqqPlot()) 
+  })
+  
+  
+  
+  info <- reactive({
+    info1 <- paste("This analysis was conducted with ", strsplit(R.version$version.string, " \\(")[[1]][1], ".", sep = "")# バージョン情報
+    info2 <- paste("It was executed on ", date(), ".", sep = "")
+    cat(sprintf(info1), "\n")
+    cat(sprintf(info2), "\n")
+  })
+  
+  output$info.out <- renderPrint({
+    info()
+  })
+  
+  
+  
+  output$textarea.out <- renderPrint({
+    bs()
+  })
+  
+  output$testnorm.out <- renderPrint({
+    testnorm()
+  })
+  
+  output$downloadDistPlot <- downloadHandler(
+    filename = function() {
+      paste('Distribution-', Sys.Date(), '.pdf', sep='')
+    },
+    content = function(FILE=NULL) {
+      pdf(file=FILE)
+      print(makedistPlot())
+      dev.off()
+    })
+  
+  output$downloadBoxPlot <- downloadHandler(
+    filename = function() {
+      paste('Boxplot-', Sys.Date(), '.pdf', sep='')
+    },
+    content = function(FILE=NULL) {
+      pdf(file=FILE)
+      print(makeboxPlot())
+      dev.off()
+    })
+  
+  output$downloadQQPlot <- downloadHandler(
+    filename = function() {
+      paste('QQplot-', Sys.Date(), '.pdf', sep='')
+    },
+    content = function(FILE=NULL) {
+      pdf(file=FILE)
+      print(makeqqPlot())
+      dev.off()
+    })
+  
+})
---
+++ ui.R
@@ -0,0 +1,78 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  
+  headerPanel("Basic Statistics Calculator"),
+  
+  
+  sidebarPanel(
+    
+    p("Data input:"),
+    tags$textarea(id="textarea.in", rows=30, cols=10, "78\n57\n66\n78\n71\n78\n80\n64\n66\n75\n68\n84\n46\n73\n48\n70\n84\n73\n64\n78\n70\n87\n71\n45\n59\n70\n64\n80\n57\n91\n84\n70\n77\n68\n75\n62\n52\n59\n71\n66\n68\n61\n64\n77\n68\n70\n46\n53\n73\n62\n53\n70\n82\n70\n77\n82\n59\n86\n78\n80\n75\n80\n82\n94\n84\n84\n91\n59\n71\n77\n86\n66\n55\n71\n71\n87\n77\n87\n82\n68\n71\n91\n71\n78\n80\n94\n82\n75\n78\n82\n52\n68"),
+    p('Input values can be separated by', br(),
+      'newlines, spaces, commas, or tabs.')
+  ),
+  
+  
+  mainPanel(
+    tabsetPanel(
+      
+      tabPanel("Main",
+               
+               h3("Basic statistics"),
+               verbatimTextOutput("textarea.out"),
+               
+               br(),
+               
+               h3("Histogram"),
+               downloadButton('downloadDistPlot', 'Download the plot as pdf'),
+               
+               plotOutput("distPlot"),
+               
+               h3("Box plot with individual data points"),
+               downloadButton('downloadBoxPlot', 'Download the plot as pdf'),
+               
+               
+               plotOutput("qqPlot", width="70%"),
+               
+               br(),
+               br(),
+               
+               strong('R session info'),
+               verbatimTextOutput("info.out")
+               
+      ),
+      
+      tabPanel("About",
+               
+               strong('Note'),
+               p('This web application is developed with',
+                 a("Shiny.", href="http://www.rstudio.com/shiny/", target="_blank"),
+                 ''),
+               
+               br(),
+               
+               strong('List of Packages Used'), br(),
+               code('library(shiny)'),br(),
+               code('library(psych)'),br(),
+               
+               br(),
+               
+
+               
+
+               
+               br(),
+               
+               a(img(src="http://i.creativecommons.org/p/mark/1.0/80x15.png"), target="_blank", href="http://creativecommons.org/publicdomain/mark/1.0/")
+      )
+    )
+  )
+))
